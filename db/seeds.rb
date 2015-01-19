063bbe69034cc07e351d12f1b70341d3d8b42d7a
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-20 03:38:20 +0600

    Seed_Dump gem added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Board.create!([
+  {title: "Playful Rose", description: "", slug: "playful-rose"},
+  {title: "The Only Men", description: "", slug: "the-only-men"},
+  {title: "Secrets of Lord", description: "", slug: "secrets-of-lord"},
+  {title: "The Stream's Fire", description: "", slug: "the-stream-s-fire"},
+  {title: "The Slaves of the Sparks", description: "", slug: "the-slaves-of-the-sparks"},
+  {title: "Touch in the Sons", description: "", slug: "touch-in-the-sons"}
+])
+Pin.create!([
+  {name: "The Naked Silk", image: "Banner08a.jpg", board_id: 1, slug: "the-naked-silk"},
+  {name: "The Eye's History", image: "bay-area-wedding-photographer-01.jpg", board_id: 4, slug: "the-eye-s-history"},
+  {name: "The Ice of the Moon", image: "Brighton_Wedding_Photographer_ruth_scott-252.jpg", board_id: 5, slug: "the-ice-of-the-moon"},
+  {name: "Fire in the Shadow", image: "IMG_4565.jpg", board_id: 6, slug: "fire-in-the-shadow"},
+  {name: "Blue Edge", image: "tumblr_m9lu16SOwm1qhwisjo1_500-fall-down-2.jpg", board_id: 1, slug: "blue-edge"},
+  {name: "Dark Dreams", image: "tumblr_mhs8zh8dg61r01dv4o1_500.jpg", board_id: 1, slug: "dark-dreams"},
+  {name: "Storms of Snow", image: "tumblr_mhb9v9LZrH1rwyxwco1_500.jpg", board_id: 3, slug: "storms-of-snow"},
+  {name: "Emperor of Fire", image: "tumblr_m7p3g1uayj1rbaqo5o1_500.jpg", board_id: 3, slug: "emperor-of-fire"},
+  {name: "The Misty's Swords", image: "images.jpeg", board_id: 4, slug: "the-misty-s-swords"}
+])
