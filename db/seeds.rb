eb4bd93afc812dc30bfe56d441e2007e7ce33e19
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-20 19:29:30 +0600

    Added sidebar.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,24 @@ Board.create!([
   {title: "Secrets of Lord", description: "", slug: "secrets-of-lord"},
   {title: "The Stream's Fire", description: "", slug: "the-stream-s-fire"},
   {title: "The Slaves of the Sparks", description: "", slug: "the-slaves-of-the-sparks"},
+  {title: "Touch in the Sons", description: "", slug: "touch-in-the-sons"},
+  {title: "Playful Rose", description: "", slug: "playful-rose"},
+  {title: "The Only Men", description: "", slug: "the-only-men"},
+  {title: "Secrets of Lord", description: "", slug: "secrets-of-lord"},
+  {title: "The Stream's Fire", description: "", slug: "the-stream-s-fire"},
+  {title: "The Slaves of the Sparks", description: "", slug: "the-slaves-of-the-sparks"},
   {title: "Touch in the Sons", description: "", slug: "touch-in-the-sons"}
 ])
 Pin.create!([
-  {name: "The Naked Silk", image: "Banner08a.jpg", board_id: 1, slug: "the-naked-silk"},
-  {name: "The Eye's History", image: "bay-area-wedding-photographer-01.jpg", board_id: 4, slug: "the-eye-s-history"},
-  {name: "The Ice of the Moon", image: "Brighton_Wedding_Photographer_ruth_scott-252.jpg", board_id: 5, slug: "the-ice-of-the-moon"},
-  {name: "Fire in the Shadow", image: "IMG_4565.jpg", board_id: 6, slug: "fire-in-the-shadow"},
-  {name: "Blue Edge", image: "tumblr_m9lu16SOwm1qhwisjo1_500-fall-down-2.jpg", board_id: 1, slug: "blue-edge"},
-  {name: "Dark Dreams", image: "tumblr_mhs8zh8dg61r01dv4o1_500.jpg", board_id: 1, slug: "dark-dreams"},
-  {name: "Storms of Snow", image: "tumblr_mhb9v9LZrH1rwyxwco1_500.jpg", board_id: 3, slug: "storms-of-snow"},
-  {name: "Emperor of Fire", image: "tumblr_m7p3g1uayj1rbaqo5o1_500.jpg", board_id: 3, slug: "emperor-of-fire"},
-  {name: "The Misty's Swords", image: "images.jpeg", board_id: 4, slug: "the-misty-s-swords"}
+  {name: "The Naked Silk", image: "tumblr_mu9xidQyWv1qiv1h8o1_500.jpg", board_id: 1, slug: "the-naked-silk"},
+  {name: "The Eye's History", image: "Palmer_Snelling_Kelly_Is_Nice_Photography_Palmer113_low.jpg", board_id: 4, slug: "the-eye-s-history"},
+  {name: "The Ice of the Moon", image: "tumblr_m4t3y5hSlI1qib5bvo1_500.jpg", board_id: 5, slug: "the-ice-of-the-moon"},
+  {name: "Fire in the Shadow", image: "Photo-of-Inspiration.jpg", board_id: 6, slug: "fire-in-the-shadow"},
+  {name: "Dark Dreams", image: "tumblr_m7p3g1uayj1rbaqo5o1_500.jpg", board_id: 1, slug: "dark-dreams"},
+  {name: "Blue Edge", image: "tumblr_mf6h6gHAnJ1rheevxo1_500.jpg", board_id: 1, slug: "blue-edge"},
+  {name: "Storms of Snow", image: "nice-photography.jpg", board_id: 3, slug: "storms-of-snow"},
+  {name: "The Misty's Swords", image: "1386042366_nice_photography_wallpaper.jpg", board_id: 4, slug: "the-misty-s-swords"},
+  {name: "Emperor of Fire", image: "tumblr_m2hsln3B2l1rtq7j6o1_500.jpg", board_id: 3, slug: "emperor-of-fire"},
+  {name: "Children of Dreams", image: "cool-girl-nice-photography-sweet-Favim.com-250633_large.jpg", board_id: 4, slug: "children-of-dreams"},
+  {name: "Children of Dreams", image: "tumblr_m2hsln3B2l1rtq7j6o1_500.jpg", board_id: 5, slug: "children-of-dreams-7d70974c-a813-4b0c-8745-3e77692fc97a"}
 ])
