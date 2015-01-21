b9a067c6434389beceefdcc0dc080c72b09e9876
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-21 16:38:23 +0600

    Removed Wookmark and Added Masonry for Pinterest Like image showing...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery.infinitescroll
-//= require jquery.wookmark
-//= require trigger_wookmark
+//= require masonry/jquery.masonry
 //= require_tree .
