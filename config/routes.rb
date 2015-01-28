e8a7bc154b7d38c7bb946c3a2992cd44186a5de3
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-29 02:15:12 +0600

    Added Omniauth - sign in with facebook.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
   post 'pin_post/:id', to: 'pins#pin_post', as: 'pin_post'
   resources :boards
   resources :pins
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   root 'pins#index'
 
